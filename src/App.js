03eb4ed761914e52e720babbb9957cfe2e7b578a
cgnnanna <cgnnanna@gmail.com>
2021-05-13 02:15:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import logo from "./images/logo.svg";
+import mockUp from "./images/illustration-mockups.svg";
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <img src={logo} alt="Header Logo"/>
       </header>
-    </div>
+      <main>
+        <img src={mockUp} alt="Mockup Logo"/>
+        <div className="content">
+          <h2>Build The Community Your Fans Will Love</h2>
+          <p>Huddle re-imagines the way we build communities.
+            You have a voice, but so does your audience. 
+            Create connections with your users and engage in genuine discussion.
+          </p>
+          <button>Register</button>
+        </div>
+      </main>
+      <footer>
+        <div className="footer-icon-wrapper">
+          <div className="icon-block"><i className="fab fa-facebook-f icon-white"></i></div>
+          <div className="icon-block"><i className="fab fa-twitter icon-white"></i></div>
+          <div className="icon-block"><i className="fab fa-instagram icon-white"></i></div>
+        </div>
+      </footer>
+    </>
   );
 }
 
